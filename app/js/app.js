6342dacb21738826914c0d31e4d9cc59efa711a0
Anton Demidenko <yaintroversivnost@gmail.com>
2021-02-20 17:07:02 +0500

    Добавил кнопку переключения темы в шапку сайта

--- app/js/app.js
+++ app/js/app.js
@@ -9,5 +9,47 @@ document.addEventListener("DOMContentLoaded", function() {
     $('.logo__img').lazyLoadXT();
   });
 
+  // Темная тема
+    // const toggleBtn = document.querySelectorAll('.btn-toggleTheme');
+    const toggleBtn = document.querySelector('.btn-toggleTheme');
+    const linkThemes = document.querySelector('#link-themes');
+
+    function checkStatusTheme () {
+
+      // Меняем значение title, цвет кнопки и css стили
+      if (toggleBtn.getAttribute('title') === 'Включить темный режим') {
+        toggleBtn.setAttribute ('title', 'Включить светлый режим');
+        toggleBtn.classList.remove('light-theme');
+        //linkThemes.setAttribute('href', 'css/dark.css'); ДОБАВИТЬ ФАЙЛ dark.css В ПРОЕКТ!!!
+
+      } else {
+        toggleBtn.setAttribute ('title', 'Включить темный режим');
+        toggleBtn.classList.add('light-theme');
+      }
+      
+    }
+
+    // Запускаем функцию при клике
+    const toggleTheme = addEventListener ('click', function() {
+      checkStatusTheme ();
+      console.log('click');
+    })
+    
+     
+      
+      
+   
+      
+  
+
+
+
+
+
+
+
+
+
+
 });
 
