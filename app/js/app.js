d11650b939f29124029fee8d0ad71224b4c4e3c0
Anton Demidenko <yaintroversivnost@gmail.com>
2021-02-22 00:36:40 +0500

    upd

--- app/js/app.js
+++ app/js/app.js
@@ -1,84 +1,74 @@
-document.addEventListener("DOMContentLoaded", function() {
-  console.log('DOM загружен успешно');
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM загружен успешно");
 
-  particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
+  particlesJS.load("particles-js", "assets/particles.json", function () {
+    console.log("callback - particles.js config loaded");
   });
 
-  $(function() {
-    $('.logo__img').lazyLoadXT();
+  $(function () {
+    $(".logo__img").lazyLoadXT();
   });
 
   // Темная тема
-    const toggleBtn = document.querySelector('.btn-toggleTheme');
-    // const linkThemes = document.querySelector('#link-themes');
+  const toggleBtn = document.querySelector(".btn-toggleTheme");
+  // const linkThemes = document.querySelector('#link-themes');
 
-    function checkStatusTheme () {
-
-      // Меняем значение title, цвет кнопки и css стили
-      if (toggleBtn.getAttribute('title') === 'Включить темный режим') {
-        toggleBtn.setAttribute ('title', 'Включить светлый режим');
-        toggleBtn.classList.remove('light-theme');
-        //linkThemes.setAttribute('href', 'css/dark.css'); ДОБАВИТЬ ФАЙЛ dark.css В ПРОЕКТ!!!
-
-      } else {
-        toggleBtn.setAttribute ('title', 'Включить темный режим');
-        toggleBtn.classList.add('light-theme');
-      }
-      
+  function checkStatusTheme() {
+    // Меняем значение title, цвет кнопки и css стили
+    if (toggleBtn.getAttribute("title") === "Включить темный режим") {
+      toggleBtn.setAttribute("title", "Включить светлый режим");
+      toggleBtn.classList.remove("light-theme");
+      //linkThemes.setAttribute('href', 'css/dark.css'); ДОБАВИТЬ ФАЙЛ dark.css В ПРОЕКТ!!!
+    } else {
+      toggleBtn.setAttribute("title", "Включить темный режим");
+      toggleBtn.classList.add("light-theme");
     }
+  }
 
-    // Запускаем функцию при клике
-    const toggleTheme = toggleBtn.addEventListener ('click', function() {
-      checkStatusTheme ();
-      console.log('click');
-    })
-    
+  // Запускаем функцию при клике
+  const toggleTheme = toggleBtn.addEventListener("click", function () {
+    checkStatusTheme();
+    console.log("click");
+  });
 
-    // Проверка на адблок
-    const adblockBox = document.querySelector('.adblock-box');
-    const alertAdb = document.querySelector('.alert-adb');
+  // Проверка на адблок
+  const adblockBox = document.querySelector(".adblock-box");
+  const alertAdb = document.querySelector(".alert-adb");
 
-    setInterval (checkAdb, 2000);
-    
-    function checkAdb () {
-      if (adblockBox.clientHeight < 1) {
-        console.log('есть адблок');
-        alertAdb.style.top = 'calc(50% - 205px)';
-        alertAdb.style.opacity = '1';
-        alertAdb.style.visibility = 'visible';
-      } else {
-        console.log('нет адблок');
-        alertAdb.style.display = 'none';
-      }
+  setInterval(checkAdb, 2000);
+
+  function checkAdb() {
+    if (adblockBox.clientHeight < 1) {
+      console.log("есть адблок");
+      alertAdb.style.top = "calc(50% - 205px)";
+      alertAdb.style.opacity = "1";
+      alertAdb.style.visibility = "visible";
+    } else {
+      console.log("нет адблок");
     }
+  }
 
-    // Закрыть окно алерт адблок
-    const alertAdbClose = document.querySelector('.alert-adb__close');
-    const alertAdbClosed = alertAdbClose.addEventListener('click', function(){
-      alertAdb.style.display = 'none';
-      if (alertAdb.style.display === 'none') {
-        clearInterval(checkAdb);// Не работает - нужно поправить
-      }
-    })
+  // Закрыть окно алерт адблок
+  const alertAdbClose = document.querySelector(".alert-adb__close");
 
-    
-    
-     
-      
-      
-   
-      
+  const alertAdbClosed = alertAdbClose.addEventListener("click", function () {
+    alertAdb.style.display = "none";
+  });
+
+  // 1 - добавим в LocalStorage элемент с ключом «bgColor» и значением «green»
+  localStorage.setItem("bgColor", "green");
+  // 2 - получим значение по ключу «bgColor» и сохраним его в переменную «bgColor»
+  var bgColor = localStorage.getItem("bgColor");
+
+
+
+
+
+
+
+
+
+
+
   
-
-
-
-
-
-
-
-
-
-
 });
-
