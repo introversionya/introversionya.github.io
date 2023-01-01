44007461d1ff351526855d31cb2b6d4e96706537
Anton Demidenko <yaintroversivnost@gmail.com>
2023-01-02 01:06:24 +0500

    all del

--- dist/assets/MainPage-3a055a52.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as a,c as n}from"./index-934dd443.js";const o={name:"MainPage"},s={class:"main"};function t(c,r,_,i,p,m){return a(),n("main",s)}const d=e(o,[["render",t]]);export{d as default};
