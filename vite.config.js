e2b3496a9f05742f4c7fd5eacf59228979fc5786
Anton Demidenko <yaintroversivnost@gmail.com>
2023-01-02 00:30:40 +0500

    правка конфига vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), legacy({
-    targets: ['IE 9'],
+    targets: ['defaults', 'not IE 11']
   })],
   resolve: {
     alias: {
@@ -16,5 +16,3 @@ export default defineConfig({
     }
   }
 })
-
-// targets: ['defaults', 'not IE 11'],
