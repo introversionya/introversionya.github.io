056f6741f43084e066e78d180b2aed4b177e65ad
Anton Demidenko <yaintroversivnost@gmail.com>
2023-01-01 23:01:03 +0500

    Vite: добавил плагин vitejs/plugin-legacy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import legacy from '@vitejs/plugin-legacy'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), legacy({
+    targets: ['IE 9'],
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
+
+// targets: ['defaults', 'not IE 11'],
