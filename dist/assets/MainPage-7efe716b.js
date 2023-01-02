d655d7cd2d5ab678db9eb80fc190734408db7ec7
Anton Demidenko <yaintroversivnost@gmail.com>
2023-01-02 21:22:19 +0500

    правка meta тегов

---
+++ dist/assets/MainPage-7efe716b.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as n}from"./index-1bf226c8.js";const o={name:"MainPage"},s={class:"main"};function t(c,r,_,i,p,m){return a(),n("main",s)}const d=e(o,[["render",t]]);export{d as default};
---
+++ dist/assets/MainPage-legacy-db1f477b.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-912af2cc.js"],(function(e,n){"use strict";var t,r,s;return{setters:[e=>{t=e._,r=e.o,s=e.c}],execute:function(){const n={class:"main"};e("default",t({name:"MainPage"},[["render",function(e,t,a,c,i,u){return r(),s("main",n)}]]))}}}));
