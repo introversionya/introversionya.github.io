a98127fdf915061fd5c6509aea08c5ed4348d8df
Anton Demidenko <yaintroversivnost@gmail.com>
2023-05-08 16:14:30 +0500

    Обработка 404

---
+++ error.vue
@@ -0,0 +1,26 @@
+<script setup>
+const { error } = defineProps(['error']);
+console.clear()
+
+useHead({
+  title: 'Ошибка',
+});
+
+// definePageMeta({
+//   title: 'Ошибка',
+// });
+
+
+// clearError()
+// const router = useRouter();
+
+// if (error.statusCode === 404) {
+//   // router.push('/404');
+//   clearError()
+// }
+</script>
+
+<template>
+  <div>Такой страницы нет:(</div>
+  {{ error.statusCode }}
+</template>
